3a779edc5696d19ceff9add0d01d72e9df814d3d
zy <379435696@qq.com>
2018-12-28 18:11:54 +0800

    update分词

--- fenci_test.py
+++ fenci_test.py
@@ -7,38 +7,45 @@ import io
 from task_data_model import ReportData
 import collections
 
-rd = ReportData()
-
-
-#读取过滤词语
+#读取过滤词语组装成数组
 def stopwordslist(filepath):
  stopwords = [line.strip() for line in io.open(filepath, 'r', encoding='utf-8').readlines()]
  return stopwords
 
 
-def fenci():
- stopwords = stopwordslist(r'C:\Users\Administrator\Desktop\python\fenci\stopwords.txt')
- #print stopwords
- orders = rd.get_words()
+def fenci_deltial():
+ #获取关键字过滤文件
+ stopwords = stopwordslist(r'C:\Users\Administrator\Desktop\python\fenci\fenci\stopwords.txt')
+ #stopwords = stopwordslist(r'/data/zy/fenci/stopwords.txt')
+
+ #获取数据
+ rd = ReportData()
+ orders = rd.get_doctor_reply()
+
+#遍历问诊对病情描述进行分词和词频统计
  for order in orders:
   word = order[1]
   result_list = psg.cut(word)
   rusult = []
   for x in result_list:
    if x.word not in stopwords and x.word.strip() != '':
-    #print x.word
      rusult.append(x.word)
-  #统计词频
-  mycount = collections.Counter(rusult)
-  words = ''
-  for k,v in mycount.items():
-   words += (k+':'+str(v))+','
-  #print words
-  rd.inser_words(order[0],words)
 
+  # 统计词频
+  mycount = collections.Counter(rusult)
+
+  # 拼接sql保存结果到数据库
+  sql = "INSERT INTO t_doctor_reply_fenci(f_order_id,f_wd,f_cnt,f_dep) VALUES"
+  if len(mycount.items()) !=0:
+   #print mycount.items(),order
+   for k, v in mycount.items():
+    sql += "('" + order[0] + "','" + k + "','" + str(v) + "','" + order[2] + "'),"
+
+   #print sql[:-1]
+   rd.insert_sql(sql[:-1])
 
 if __name__ == "__main__":
- fenci()
+ fenci_deltial()
 
 
 
